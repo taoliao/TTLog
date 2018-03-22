73e5dc1d2ab96a282c56ff795bcc0748d9b62cff
LiaoTao <TL163.com>
2018-03-22 16:26:04 +0800

    全局打印

---
+++ TTLog/TTLog/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  TTLog
+//
+//  Created by corepress on 2018/3/22.
+//  Copyright © 2018年 corepress. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    TLLog(@"eyfedfjldk");
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
